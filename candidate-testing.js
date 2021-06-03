cc24b1d2888e0691eb17d11484ad3d4779b06f98
maks075 <>
2021-06-03 18:22:59 +0000

    1.2c

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,7 +15,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("Candidate Name:")
+candidateName = input.question("Candidate Name: ")
 }
 
 function askQuestion() {
@@ -28,8 +28,8 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
+console.log("Your Answer: ", candidateAnswer);
+console.log("Correct Answer: ", correctAnswer);
   let grade;
   
 
